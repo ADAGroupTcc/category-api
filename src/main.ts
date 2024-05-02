7e436d59d5535032d348b4605553e51d1a7f89f2
RafalFDS2 <rafael.ferraz@picpay.com>
2024-05-01 22:09:43 -0300

    ambient

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+  );
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(process.env.PORT);
+}
+bootstrap();
