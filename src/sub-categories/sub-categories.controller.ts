216a27174ccabf778cbd0d64192d2a04d81b93d8
RafalFDS2 <rafael.ferraz@picpay.com>
2024-05-21 22:46:44 -0300

    feat: create sub-category + fix: verify if the subcategory exists in categories'creation

--- src/sub-categories/sub-categories.controller.ts
+++ src/sub-categories/sub-categories.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Header, Headers, Post } from '@nestjs/common';
+import { SubCategoryDto } from './dto';
+import { SubCategoriesService } from './sub-categories.service';
 
-@Controller('v1/categories/:category_id/sub-categories')
-export class SubCategoriesController { }
+@Controller('v1/sub-categories')
+export class SubCategoriesController {
+
+  constructor(private readonly subCategoriesService: SubCategoriesService) { }
+
+  @Post()
+  async createSubCategory(@Body() body: SubCategoryDto, @Headers('user_id') userId: string) {
+    if (!userId || userId === '') {
+      throw new BadRequestException('Missing user_id header.');
+    }
+    body.creatorId = userId;
+    return await this.subCategoriesService.create(body);
+  }
+}
